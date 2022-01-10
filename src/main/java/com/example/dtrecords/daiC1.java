e5f64d0b031a8aa6ca1180e279d09eae66874fcc
daitt <daitt@toprate.io>
2022-01-10 16:07:26 +0700

    test add command

---
+++ src/main/java/com/example/dtrecords/daiC1.java
@@ -0,0 +1,5 @@
+package com.example.dtrecords;
+
+public class daiC1 {
+
+}
---
+++ src/main/java/com/example/dtrecords/gitTu.java
@@ -0,0 +1,7 @@
+package com.example.dtrecords;
+
+public class gitTu {
+    private Integer id;
+
+    private String name;
+}
